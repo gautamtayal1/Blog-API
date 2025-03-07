d4fc3ca2987463758e916d32ccb3604596eb9f43
Gautam Tayal <gautamtayal65@gmail.com>
2025-03-07 13:34:18 +0530

    backend complete

--- backend/src/controllers/authorCommentController.js
+++ backend/src/controllers/authorCommentController.js
@@ -1,14 +1,44 @@
+const prisma = require("../config/db.config")
 
-const getComments = async(req, res) => {
+const getComments = async (req, res) => {
+  try {
+    const userId = req.user?.id;
 
-}
+    const comments = await prisma.comment.findMany({
+      where: {
+        blog: {
+          userId: userId // Only fetch comments for blogs written by this user
+        }
+      },
+    })
+
+    res.status(200).json(comments);
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 const deleteComments = async(req, res) => {
-
+  const {id} = req.body
+  const deletedComment = await prisma.comment.delete({
+    where:{
+      id:id
+    }
+  })
+  res.send(deletedComment)
 }
 
 const updateComments = async(req, res) => {
-
+  const {id, comment} = req.body
+  const updatedComment = await prisma.comment.update({
+    where: {
+      id:id
+    }, data : {
+      comment
+    }
+  }) 
+  res.send(updatedComment)
 }
 
 
